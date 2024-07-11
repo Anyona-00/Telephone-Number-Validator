7509b49886a0729ddb2c0bf5d131cfc3d59ec644
Anyona-00 <ombasaanyona@gmail.com>
2024-07-11 23:25:26 +0300

    Completed the regex Expresions

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ const resultDiv = document.getElementById("results-div");
 
 
 const checkUserInput = () => {
-  if (userInput.value === "") {
+  const input = userInput.value.trim();
+
+  if (input === "") {
     alert("Please provide a phone number");
+    return;
+  }
+
+  let regexes = [
+    /^1 \d{3}-\d{3}-\d{4}$/,
+    /^1 \(\d{3}\) \d{3}-\d{4}$/,
+    /^\d{10}$/,
+    /^\d{3}-\d{3}-\d{4}$/,
+    /^\(\d{3}\)\d{3}-\d{4}$/,
+    /^1\(\d{3}\)\d{3}-\d{4}$/,
+    /^1 \d{3} \d{3} \d{4}$/,
+  ];
+
+  let isValid = false;
+
+  for (let i = 0; i < regexes.length; i++) {
+    if (regexes[i].test(input)) {
+      isValid = true;
+      break;
+    }
+  }
+
+  if (isValid) {
+    resultDiv.textContent = `Valid US number: ${input}`;
+  } else {
+    resultDiv.textContent = `Invalid US number: ${input}`;
   }
 };
 
@@ -15,6 +43,7 @@ const checkUserInput = () => {
 
 const clear = () => {
   resultDiv.textContent = "";
+  userInput.value = "";
 };
 
 checkBtn.addEventListener("click", checkUserInput);
